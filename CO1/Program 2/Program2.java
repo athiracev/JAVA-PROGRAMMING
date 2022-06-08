7ee2521b06742b2affbf28032e8068d5b76421ca
ATHIRA P <94682674+athiracev@users.noreply.github.com>
2022-06-08 21:58:56 +0530

    Update Program2.java

--- CO1/Program 2/Program2.java
+++ CO1/Program 2/Program2.java
@@ -1,62 +1,89 @@
 //  Matrix Addition
 
 import java.util.Scanner;
+import java.io.*;
+class Matrix
+{
+	
+		
+		int r1,c1,r2,c2,i,j;
+		int m1[][] = new int[10][10];
+		int m2[][] = new int[10][10];
+		int s=0;
+			
+			
+	void getData()
+	{
+		Scanner k =new Scanner(System.in);
+		System.out.println("enter first matrix row and column value");
+		r1 = k.nextInt();
+		c1 = k.nextInt();
+		System.out.println("enter second matrix row and column value");
+		r2 = k.nextInt();
+		c2 = k.nextInt();
+		System.out.println("enter first matrix values");
+		for(i=0;i<r1;i++)
+		{
+			for(j=0;j<c1;j++)
+			{
+				m1[i][j]=k.nextInt();
+			}
+		}
+		System.out.println("enter second matrix values");
+		for(i=0;i<r2;i++)
+		{
+			for(j=0;j<c2;j++)
+			{
+				m2[i][j]=k.nextInt();
+			}
+		}
+		System.out.println("two Matrixes are");
+		for(i=0;i<r1;i++)
+		{
+			for(j=0;j<c1;j++)
+			{
+				System.out.print(m1[i][j]+"\t");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+		for(i=0;i<r2;i++)
+		{
+			for(j=0;j<c2;j++)
+			{
+				System.out.print(m2[i][j]+"\t");
+			}
+			System.out.println("");
+		}
+		System.out.println("");
+		if(r1 == r2 && c1 == c2)
+		{
+			System.out.println("sum of matrix is");
+			for(i=0;i<r1;i++)
+			{
+				for(j=0;j<c2;j++)
+				{
+					s = m1[i][j] + m2[i][j];
+					System.out.print(s+"\t");
+				}
+				System.out.println("");
+			}
+		
+		}
+		else
+		{
+			System.out.println("addition cannot perform");
+			
+		}
+	}
 
-class Matrix {
-    int col;
-    int row;
-    int[][] matrix;
-
-    Matrix(int r, int c) {
-        col = c;
-        row = r;
-        matrix = new int[row][col];
-    }
-
-    void matrixCreation(Scanner read) {
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                matrix[i][j] = read.nextInt();
-            }
-        }
-    }
 }
-
-public class Program2 {
-
-    public static void addMatrix(Matrix m1, Matrix m2, Matrix result) {
-        for (int i = 0; i < m1.row; i++) {
-            for (int j = 0; j < m1.col; j++) {
-                result.matrix[i][j] = m1.matrix[i][j] + m2.matrix[i][j];
-            }
-        }
-    }
-
-    public static void displayMatrix(Matrix result) {
-        System.out.println("The metrix after adding given 2 matrices:");
-        for (int i = 0; i < result.row; i++) {
-            for (int j = 0; j < result.col; j++) {
-                System.out.print("\t" + result.matrix[i][j]);
-            }
-            System.out.print("\n");
-        }
-    }
-
-    public static void main(String[] args) {
-        int row, col;
-        Scanner read = new Scanner(System.in);
-        System.out.print("Enter Number of Rows:");
-        row = read.nextInt();
-        System.out.print("Enter Number of columns:");
-        col = read.nextInt();
-        Matrix m1 = new Matrix(row, col);
-        Matrix m2 = new Matrix(row, col);
-        Matrix result = new Matrix(row, col);
-        System.out.println("Enter the elements of matrix1:");
-        m1.matrixCreation(read);
-        System.out.println("Enter the elements of matrix2:");
-        m2.matrixCreation(read);
-        addMatrix(m1, m2,result);
-        displayMatrix(result);
-    }
+class Program2
+{
+	public static void main(String a[])
+	{
+		 Matrix m=new Matrix();
+		 m.getData();
+	}
+	
 }
